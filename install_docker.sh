a2c5213b885d51953c6e6fb31ca241b1bfd5cb52
qq20004604 <20004604@qq.com>
2019-04-18 23:36:07 +0800

    更新 docker安装 脚本

---
+++ install_docker.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+echo "卸载老版本 Docker"
+sudo yum remove docker \
+                docker-client \
+                docker-client-latest \
+                docker-common \
+                docker-latest \
+                docker-latest-logrotate \
+                docker-logrotate \
+                docker-engine
+
+echo "【2】安装依赖包"
+sudo yum install -y yum-utils \
+	device-mapper-persistent-data \
+  	lvm2
+
+echo "【3】添加源"
+sudo yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+
+echo "【4】安装Docker CE版"
+sudo yum install docker-ce docker-ce-cli containerd.io
+
+echo "【5】查看Docker版本"
+docker version
